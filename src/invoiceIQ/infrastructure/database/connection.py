5c4a0fd00e46e731db324a08de47262d8e608227
Samrudh Sangalad <samrudh1307@gmail.com>
2026-08-03 23:40:50 +0530

    feat: add SQLAlchemy database foundation

---
+++ src/invoiceIQ/infrastructure/database/connection.py
@@ -0,0 +1,25 @@
+from sqlalchemy import create_engine
+from invoiceIQ.infrastructure.database.models import Base,Vendor, Invoice, LineItem
+from sqlalchemy.orm import sessionmaker
+engine = create_engine("sqlite:///invoiceiq.db")
+Base.metadata.create_all(engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+session = SessionLocal()
+vendor = Vendor(
+    legal_name="ABC Technologies",
+    tax_id="GST123",
+    address="Bengaluru",
+    contact_information="John Doe, john.doe@abc.com"
+)
+
+try:
+    session.add(vendor)
+    session.commit()
+except:
+    session.rollback()
+    raise
+finally:
+    print(vendor.vendor_id)
+    session.close()
---
+++ src/invoiceIQ/infrastructure/database/models.py
@@ -0,0 +1,40 @@
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import String, Integer, Float, Date, ForeignKey
+from datetime import date
+
+class Base(DeclarativeBase):
+    pass
+
+class Vendor(Base):
+    __tablename__ = "vendor"
+    vendor_id: Mapped[int] = mapped_column(primary_key=True)
+    legal_name: Mapped[str] = mapped_column(String(100))
+    tax_id: Mapped[str] = mapped_column(String(50))
+    address: Mapped[str] = mapped_column(String(200))
+    contact_information: Mapped[str] = mapped_column(String(100))
+
+
+class Invoice(Base):
+    __tablename__ = "invoice"
+    invoice_id: Mapped[int] = mapped_column(primary_key=True)
+    invoice_number: Mapped[str] = mapped_column(String(50))
+    vendor_id: Mapped[int] = mapped_column(ForeignKey("vendor.vendor_id"))
+    invoice_date: Mapped[date] = mapped_column(Date)
+    due_date: Mapped[date] = mapped_column(Date)
+    currency: Mapped[str] = mapped_column(String(10))
+    purchase_order_number: Mapped[str] = mapped_column(String(50))
+    payment_terms: Mapped[str] = mapped_column(String(50))
+    line_items: Mapped[list] = mapped_column(ForeignKey("line_item.line_item_id"))
+    tax_total: Mapped[float] = mapped_column(Float)
+    subtotal: Mapped[float] = mapped_column(Float)
+    shipping_total: Mapped[float] = mapped_column(Float)
+    grand_total: Mapped[float] = mapped_column(Float)
+
+class LineItem(Base):
+    __tablename__ = "line_item"
+    line_item_id: Mapped[int] = mapped_column(primary_key=True)
+    invoice_id: Mapped[int] = mapped_column(ForeignKey("invoice.invoice_id"))
+    description: Mapped[str] = mapped_column(String(200))
+    quantity: Mapped[float] = mapped_column(Float)
+    unit_price: Mapped[float] = mapped_column(Float)
+    total_price: Mapped[float] = mapped_column(Float)
