a90baf1b8eed797503bb48501a2e88283ae7e8f2
Samrudh Sangalad <samrudh1307@gmail.com>
2026-08-04 23:58:05 +0530

    read and write vendor

--- src/invoiceIQ/infrastructure/database/connection.py
+++ src/invoiceIQ/infrastructure/database/connection.py
@@ -1,6 +1,9 @@
 from sqlalchemy import create_engine
 from invoiceIQ.infrastructure.database.models import Base,Vendor, Invoice, LineItem
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import select
+
+
 engine = create_engine("sqlite:///invoiceiq.db")
 Base.metadata.create_all(engine)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -23,3 +26,18 @@ except:
 finally:
     print(vendor.vendor_id)
     session.close()
+
+statement = select(Vendor)
+
+result = session.execute(statement)
+
+vendors = result.scalars().all()
+
+for vendor in vendors:
+    print(vendor.vendor_id)
+    print(vendor.legal_name)
+statement = select(Vendor).where(Vendor.vendor_id == 1)
+
+vendor = session.execute(statement).scalar_one_or_none()
+
+print(vendor)
